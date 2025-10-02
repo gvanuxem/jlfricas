0a90d8b2d54b541e19985e2f9307672c2d7610aa
gvanuxem <g.vanuxem@gmail.com>
2025-10-02 07:23:17 +0200

    Change html documentation style to not avoid confusion with official FriCAS documentation

--- src/doc/sphinx/source/conf.py
+++ src/doc/sphinx/source/conf.py
@@ -103,7 +103,7 @@ highlight_language = 'none'
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'fricas-theme'
+html_theme = 'bizstyle'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -115,7 +115,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_theme']
+#html_theme_path = ['_theme']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
